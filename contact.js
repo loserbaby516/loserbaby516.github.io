a0398f1e188f3d352342b8c72e8b0bb6f60d1fb6
loserbaby516 <205492338+loserbaby516@users.noreply.github.com>
2025-05-05 17:45:54 -0400

    hcaptcha?

--- contact.js
+++ contact.js
@@ -13,7 +13,26 @@ async function record_form() {
         console.error(e);
     } 
 }
-  
+
+function check_hcaptcha() {
+    SECRET_KEY = "ES_5f9d9bc9e16141fba01c6699f377fbc2"
+    VERIFY_URL = "https://api.hcaptcha.com/siteverify"
+
+    token = request.POST_DATA['h-captcha-response']
+
+    data = { 'secret': SECRET_KEY, 'response': token }
+
+    response = http.post(url=VERIFY_URL, data=data)
+
+    response_json = JSON.parse(response.content)
+
+    success = response_json['success']
+}
+
+
+
+
+
 function thankYou() {
     event.preventDefault;
     alert('Thank you for reaching out!');
