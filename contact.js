789e2ee6ca8c5a994345a5b530f79b3904f539ee
loserbaby516 <205492338+loserbaby516@users.noreply.github.com>
2025-05-05 11:15:16 -0400

    edits

--- contact.js
+++ contact.js
@@ -1,4 +1,22 @@
-function thankYou() {
-    alert('Thank you for reaching out!');
+function record_form() {
     event.preventDefault();
+    const sub = new FormData(form);
+    for (item of sub) {
+        console.log(item);
+    }
 }
+  
+function thankYou() {
+    event.preventDefault;
+    alert('Thank you for reaching out!');
+}
+  
+
+  /* const form = document.querySelector('#form');
+  form.addEventListener('submit', function() {
+      event.preventDefault();
+      const sub = new FormData(form);
+      console.log(sub);
+  }) */
+
+
